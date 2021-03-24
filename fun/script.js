f166a0204d8cd6ff025193af24d0bc8e129a67e5
Soumyadeep <78623094+python9160@users.noreply.github.com>
2021-03-24 08:39:46 -0400

    rip rick didnt work

--- fun/script.js
+++ fun/script.js
@@ -6,6 +6,6 @@ $("#plsdo").click(() => {
   window.open(window.location.origin, "_blank", "width=1000, height=600");
 });
 
-$("lololol").click(() => {
-  window.open('https://tinyurl.com/nvej2mk4')
+$("#lololol").click(() => {
+  window.open('https://tinyurl.com/nvej2mk4', '_self')
 });

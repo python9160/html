373c4ee8690d87dc904b6056ac70e88cc9e0954e
Soumyadeep <78623094+python9160@users.noreply.github.com>
2021-03-24 08:29:13 -0400

    rickrol bois

--- fun/script.js
+++ fun/script.js
@@ -5,3 +5,7 @@ $("#plsdont").click(() => {
 $("#plsdo").click(() => {
   window.open(window.location.origin, "_blank", "width=1000, height=600");
 });
+
+$("lololol").click(() => {
+  window.open('https://tinyurl.com/nvej2mk4')
+});

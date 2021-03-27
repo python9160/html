9507dcbe262b114cdc69ed08ba7bd06346bcffa3
unknown <gaurav.perugu@gmail.com>
2021-03-26 23:03:40 -0400

    dk

--- fun/script.js
+++ fun/script.js
@@ -14,3 +14,7 @@ $("#lololol").click(() => {
 $("#truform").click(() => {
   window.open("https://www.bing.com/images/search?view=detailV2&ccid=UGTHK6nn&id=AE3FB4075E0F93A1C60A708901E394040597E857&thid=OIP.UGTHK6nnoI4W4A2T1rYkmgHaFT&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR5064c72ba9e7a08e16e00d93d6b6249a%3frik%3dV%252biXBQSU4wGJcA%26riu%3dhttp%253a%252f%252fwww.notrashcan.com%252fwp-content%252fuploads%252fDirty_trash_can_overflowing.jpg%26ehk%3dv7TWW%252b56xB5UV4l0kGUbph4hsZ%252biw5lQ%252bLMmG3lz4Vw%253d%26risl%3d%26pid%3dImgRaw&exph=322&expw=450&q=trash+can&simid=608053986341235778&ck=13DE67BA69227C62F7A0E949EFFAAC14&selectedIndex=23&FORM=IRPRST&ajaxhist=0", "_blank");
 });
+
+$("#clickforacookie").click(() => { 
+  alert("uhm go get a cookie");
+});

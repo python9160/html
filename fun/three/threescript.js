586a557cc4c6912f4e264b741717bced50506497
Soumyadeep <rana.das10011@gmail.com>
2021-04-06 19:34:39 +0000

    m

--- fun/three/threescript.js
+++ fun/three/threescript.js
@@ -1,4 +1,4 @@
-import * as THREE from "three.js";
+const THREE = require("./three.js")();
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
